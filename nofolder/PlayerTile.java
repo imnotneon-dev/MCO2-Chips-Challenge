a47e05d5b896f3d6501150e6556f3efdcdcba888
ryzdevl <ryan_malapitan@dlsu.edu.ph>
2025-11-24 23:54:46 +0800

    feat: added tile for player symbol

--- nofolder/PlayerTile.java
+++ nofolder/PlayerTile.java
@@ -4,10 +4,11 @@ public class PlayerTile extends Tiles {
     public PlayerTile() {
         super('@');
         this.sprite = new ImageIcon("Chip.png"); // Make sure this image exists
-    } //overwritten code sample
+    } 
     
     @Override
     public boolean isWalkable(Chip chip, Maps map, Inventory inv, int requiredChips) {
         return false; // Other entities can't walk through the player
     }
+
 }
