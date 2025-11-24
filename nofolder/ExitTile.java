56fb6af5c68c637f69fa0280b83e5f5261d9153a
ryzdevl <ryan_malapitan@dlsu.edu.ph>
2025-11-24 15:33:03 +0800

    fix: removed onstep (temp), items now inherit tiles

--- nofolder/ExitTile.java
+++ nofolder/ExitTile.java
@@ -4,12 +4,11 @@ public class ExitTile extends Tiles {
     
     public ExitTile() {
         super('E');
-        sprite = (new ImageIcon("images/ExitTile.png"));
+        sprite = (new ImageIcon("ExitTile.png"));
     }
 
     @Override
-    public boolean isWalkable(Inventory inv, int requiredChips) {
+    public boolean isWalkable(Chip chip, Maps map, Inventory inv, int requiredChips) {
         return inv.getChips() >= requiredChips;
     }
-
 }
