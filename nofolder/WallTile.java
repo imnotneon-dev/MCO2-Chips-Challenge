c1ad59dcb7cf7cd3baffcd48407b7e7870f80858
ryzdevl <ryan_malapitan@dlsu.edu.ph>
2025-11-24 15:35:12 +0800

    fix: removed onstep (temp), items now inherit tiles

--- nofolder/WallTile.java
+++ nofolder/WallTile.java
@@ -4,11 +4,11 @@ public class WallTile extends Tiles {
     
     public WallTile() {
         super('X');
-        sprite = new ImageIcon("images/WallTile.png");
+        sprite = new ImageIcon("WallTile.png");
     }
 
-    @Override
-    public boolean isWalkable(Inventory inv, int requiredChips) {
+    @Override 
+    public boolean isWalkable(Chip chip, Maps map, Inventory inv, int requiredChips) {
         return false;
     }
 }
