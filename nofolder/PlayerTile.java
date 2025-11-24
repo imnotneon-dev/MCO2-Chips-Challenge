4cd77235a593ef92ce49263a83f4fb2f477de92d
ryzdevl <ryan_malapitan@dlsu.edu.ph>
2025-11-24 23:53:29 +0800

    test overwrite

---
+++ nofolder/PlayerTile.java
@@ -0,0 +1,13 @@
+import javax.swing.ImageIcon;
+
+public class PlayerTile extends Tiles {
+    public PlayerTile() {
+        super('@');
+        this.sprite = new ImageIcon("Chip.png"); // Make sure this image exists
+    }
+    
+    @Override
+    public boolean isWalkable(Chip chip, Maps map, Inventory inv, int requiredChips) {
+        return false; // Other entities can't walk through the player
+    }
+}
